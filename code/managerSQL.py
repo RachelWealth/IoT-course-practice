40133ecb8da5425aa630b46625352a3678ff00ab
lixueyy <2791964362@qq.com>
2021-09-18 11:22:53 +0800

    Encapsulation of the database
    Completed the encapsulation of database operations

--- code/managerSQL.py
+++ code/managerSQL.py
@@ -1,49 +1,136 @@
 import sqlite3
 import datetime
-
 now_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-clothes=[(1,"clothe","male","red","spring","lining",now_time,now_time,0),
-         (2,"clothe","male","blue","winner","bosideng",now_time,now_time,0)]
-flavorings=[]
-books=[]
-dustbins=[]
 
-con=sqlite3.connect(r"..\system.db")
-#con.execute("create table clothe(id integer primary key,type,gender,color,season,brand,create_time,updata_time,deleted integer)")
-#con.executemany("INSERT INTO clothe VALUES(?,?,?,?,?,?,?,?,?)",clothes)
+class managerSQL():
+    def __init__(self):
+        """
+            初始化函数，创建数据库连接
+        """
+        self.con = sqlite3.connect(r"../IoT.db")
+        self.cursor = self.con.cursor()
 
-con.execute("create table flavoring(id integer primary key,type,brand,create_time,updata_time,deleted integer)")
-con.executemany("INSERT INTO flavoring VALUES(?,?,?,?,?,?,?,?,?)",flavorings)
+    def createTable(self):
+        """
+            数据库中表的创建，共三个表，分别为clothe,flavoring,book
+        """
+        try:
+            self.cursor.execute("create table clothe (id integer primary key,type varchar,gender varchar,color varchar,season varchar,brand varchar,create_time,updata_time,deleted integer)")
+            self.cursor.execute("create table flavoring (id integer primary key,type varchar,brand varchar,create_time,updata_time,deleted integer)")
+            self.cursor.execute("create table book (id integer primary key,type varchar,author varchar,language varchar,publisher varchar,create_time,updata_time,deleted integer)")
+            self.con.commit()
+        except Exception as e:
+            self.con.rollback()
 
-con.execute("create table book(id integer primary key,type,author,language,publisher,create_time,updata_time,deleted integer)")
-con.executemany("INSERT INTO book VALUES(?,?,?,?,?,?,?,?,?)",books)
+    def executeInsertClothe(self,id,gender,color,season,brand):
+        """
+            数据库中clothe表的插入操作
+            @:param id: 唯一标识
+            @:param gender: 性别
+            @:param color: 颜色
+            @:param season: 季节
+            @:param brand: 品牌
+        """
+        sql='insert into clothe(id,type,gender,color,season,brand,create_time,updata_time,deleted) values(?,?,?,?,?,?,?,?,?)'
+        try:
+            self.cursor.execute(sql, (id,"clothe",gender,color,season,brand,now_time,now_time,0))
+            self.con.commit()
+        except Exception as e:
+            self.con.rollback()
 
-con.execute("create table dustbin(id integer primary key,type,type_id)")
-con.executemany("INSERT INTO book VALUES(?,?,?,?,?,?,?,?,?)",dustbins)
-con.commit()
+    def executeInsertFlavoring(self,id,brand):
+        """
+            数据库中flavoring表的插入操作
+            @:param id: 唯一标识
+            @:param brand: 品牌
+        """
+        sql='insert into flavoring(id,type,brand,create_time,updata_time,deleted) values(?,?,?,?,?,?)'
+        try:
+            self.cursor.execute(sql, (id,"flavoring",brand,now_time,now_time,0))
+            self.con.commit()
+        except Exception as e:
+            self.con.rollback()
 
-cur = con.execute("select id,gender,type,color,season,brand,create_time,updata_time,deleted from clothe")
-for row in cur:   #循环输出结果
-    print(row)
+    def executeInsertBook(self,id,author,language,publisher):
+        """
+            数据库中book表的插入操作
+            @:param id: 唯一标识
+            @:param author: 作者
+            @:param language: 语言
+            @:param publisher: 出版社
+        """
+        sql='insert into book(id,type,author,language,publisher,create_time,updata_time,deleted) values(?,?,?,?,?,?,?,?)'
+        try:
+            self.cursor.execute(sql, (id,'book',author,language,publisher,now_time,now_time,0))
+            self.con.commit()
+        except Exception as e:
+            self.con.rollback()
 
-#以下是修改数据库字段的测试：
-#con.execute("update clothe set gender=? where id=?", ('female','2'))
-#con.execute("update clothe set updata_time=? where id=?", (now_time,'2'))
-#con.commit()
-#cur = con.execute("select id,gender,type,color,season,brand,create_time,updata_time,deleted from clothe")
-#for row in cur:   #循环输出结果
-#    print(row)
+    def executeUpdate(self,table_name,id,key,value):
+        """
+            数据库的修改
+            @:param table_name: 需要进行更新操作的表名
+            @:param id: 唯一标识，根据id进行数据库的修改
+            @:param key: 需要修改的字段名
+            @:param value: 更新后的值
+        """
+        sql='update '+table_name+' set '+key+' =?,updata_time=? where id=?'
+        try:
+            self.cursor.execute(sql, (value,now_time,id))
+            self.con.commit()
+        except Exception as e:
+            self.con.rollback()
+
+    def executeDelete(self,table_name,id):
+        """
+            数据库的删除
+            @:param table_name: 需要进行删除操作的表名
+            @:param id: 唯一标识，根据id进行数据库的删除
+        """
+        sql='delete from '+table_name+' where id=?'
+        try:
+            self.cursor.execute(sql, (id,))
+            self.con.commit()
+        except Exception as e:
+            self.con.rollback()
+
+    def executeQuery1(self, table_name):
+        """
+            数据库的无条件查询
+            @:param table_name: 需要进行查询操作的表名
+        """
+        sql = 'select * from ' + table_name
+        cur = self.cursor.execute(sql)
+        for row in cur:
+            print(row)
+
+    def executeQuery2(self, table_name, id):
+        """
+            数据库的查询
+            @:param table_name: 需要进行查询操作的表名
+            @:param id: 唯一标识,根据id进行数据库的查询
+        """
+        sql = 'select * from '+table_name+' where id = ? '
+        cur=self.cursor.execute(sql,(id,))
+        for row in cur:
+            print(row)
+
+    def close(self):
+        self.cursor.close()
+        self.con.close()
+
 """
-def updataDB(id,k,v)#id为要更新字段的id，k为要更新的字段名，v为更新后的值
-    cursor = db.cursor()
-    try:
-        update1 = "update clothe set "+k+"='"+v+"' where id="+id
-        update2 = "update clothe set updata_time=now_time where id="+id
-        cursor.execute(update)
-        print('数据更新成功')
-        db.commit()#提交数据
-    except:
-        print('数据更新失败')
-        db.rollback()
-    cursor.close()
+    以下是测试代码
 """
+if __name__ == '__main__':
+    db = managerSQL()
+    db.createTable()
+    db.executeInsertBook(1,'maijia','Chinese','suibian')
+    db.executeInsertBook(2,'antusheng','English','suibian')
+    db.executeQuery1('book')
+    db.executeQuery2('book',2)
+    db.executeUpdate('book',2,'publisher','111')
+    db.executeQuery2('book',2)
+    db.executeDelete('book',1)
+    db.executeQuery1('book')
+    db.close()
